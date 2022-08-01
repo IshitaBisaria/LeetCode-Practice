c0156c6c3207c3452f477c790775cc714c685482
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2022-08-01 09:16:28 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (75.04%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int dp[102][102];
+    int func(int i, int j, int m, int n) {
+        if(i == m-1 && j == n-1)
+            return dp[i][j] = 1;
+        if(i >= m || j >= n)
+            return 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        return dp[i][j] = func(i+1, j, m, n) + func(i, j+1, m, n);
+    }
+    int uniquePaths(int m, int n) {
+        memset(dp, -1, sizeof(dp));
+        return func(0, 0, m, n);
+    }
+};
