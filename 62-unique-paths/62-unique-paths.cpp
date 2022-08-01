aa76edbd574efeea0037d0795c463d78e7f42fec
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2022-08-01 09:26:04 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int dp[102][102];
-    int func(int i, int j, int m, int n) {
-        if(i == m-1 && j == n-1)
-            return dp[i][j] = 1;
-        if(i >= m || j >= n)
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        return dp[i][j] = func(i+1, j, m, n) + func(i, j+1, m, n);
-    }
     int uniquePaths(int m, int n) {
-        memset(dp, -1, sizeof(dp));
-        return func(0, 0, m, n);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        for(int i = 0; i < m; i++)
+            dp[i][0] = 1;
+        for(int i = 0; i < n; i++)
+            dp[0][i] = 1;
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
