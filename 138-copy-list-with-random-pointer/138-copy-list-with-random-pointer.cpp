4e4bf52f18ebbf2e3bf6f1484b6b754260680482
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2022-09-16 00:14:26 +0530

    Time: 17 ms (36.53%), Space: 11.3 MB (27.86%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,37 +17,66 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(head == NULL)
-            return head;
-        Node* nHead = new Node(head->val);
-        Node* temp = head->next;
-        Node* curr = nHead;
-        while(temp != NULL) {
-            curr->next = new Node(temp->val);
-            curr = curr->next;
-            temp = temp->next;
+        Node* curr = head;
+        Node* next = NULL;
+        int i = 0;
+        while(curr != NULL) {
+            next = curr->next;
+            Node* temp = new Node(curr->val);
+            curr->next = temp;
+            temp->next = next;
+            curr = next;
         }
-        // Node* r = nHead;
-        // while(r != NULL) {
-        //     cout << r->val << " ";
-        //     r = r->next;
+        curr = head;
+        while(curr != NULL) {
+            if(curr->random != NULL) {
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+        curr = head;
+        Node* nHead = new Node(0);
+        Node* copy, *copyItr = nHead;
+        while(curr != NULL) {
+            next = curr->next->next;
+            copy = curr->next;
+            copyItr->next = copy;
+            copyItr = copy;
+            curr->next = next;
+            curr = next;
+        }
+        return nHead->next;
+        // if(head == NULL)
+        //     return head;
+        // Node* nHead = new Node(head->val);
+        // Node* temp = head->next;
+        // Node* curr = nHead;
+        // while(temp != NULL) {
+        //     curr->next = new Node(temp->val);
+        //     curr = curr->next;
+        //     temp = temp->next;
         // }
-        unordered_map<Node*, Node*> mp;
-        temp = head, curr = nHead;
-        while(temp != NULL) {
-            mp.insert({temp, curr});
-            temp = temp->next;
-            curr = curr->next;
-        }
-        // for(auto x : mp) {
-        //     cout << x.first->val << " " << x.second->val << endl;
+        // // Node* r = nHead;
+        // // while(r != NULL) {
+        // //     cout << r->val << " ";
+        // //     r = r->next;
+        // // }
+        // unordered_map<Node*, Node*> mp;
+        // temp = head, curr = nHead;
+        // while(temp != NULL) {
+        //     mp.insert({temp, curr});
+        //     temp = temp->next;
+        //     curr = curr->next;
         // }
-        temp = head;
-        while(temp != NULL) {
-            Node* t = mp[temp];
-            t->random = mp[temp->random];
-            temp = temp->next;
-        }
-        return nHead;
+        // // for(auto x : mp) {
+        // //     cout << x.first->val << " " << x.second->val << endl;
+        // // }
+        // temp = head;
+        // while(temp != NULL) {
+        //     Node* t = mp[temp];
+        //     t->random = mp[temp->random];
+        //     temp = temp->next;
+        // }
+        // return nHead;
     }
 };
