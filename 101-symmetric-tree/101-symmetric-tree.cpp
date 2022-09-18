415f373e55b3e614629cbf849c78a4e90d621d5c
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2022-09-18 09:45:58 +0530

    Time: 11 ms (40.86%), Space: 16.5 MB (47.15%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -11,26 +11,34 @@
  */
 class Solution {
 public:
+    bool func(TreeNode* left, TreeNode* right) {
+        if(!left || !right)
+            return left == right;
+        if(left->val != right->val)
+            return false;
+        return func(left->left, right->right) && func(left->right, right->left);
+    }
     bool isSymmetric(TreeNode* root) {
-        queue<TreeNode*> q;
-        q.push(root->left);
-        q.push(root->right);
-        while(!q.empty()) {
-            TreeNode* curr1 = q.front();
-            q.pop();
-            TreeNode* curr2 = q.front();
-            q.pop();
-            if((curr1 && !curr2) || (curr2 && !curr1))
-                return false;
-            if(curr1 && curr2) {
-                if(curr1->val != curr2->val)
-                    return false;
-                q.push(curr1->left);
-                q.push(curr2->right);
-                q.push(curr1->right);
-                q.push(curr2->left);
-            }
-        }
-        return true;
+        return !root || func(root->left, root->right);
+        // queue<TreeNode*> q;
+        // q.push(root->left);
+        // q.push(root->right);
+        // while(!q.empty()) {
+        //     TreeNode* curr1 = q.front();
+        //     q.pop();
+        //     TreeNode* curr2 = q.front();
+        //     q.pop();
+        //     if((curr1 && !curr2) || (curr2 && !curr1))
+        //         return false;
+        //     if(curr1 && curr2) {
+        //         if(curr1->val != curr2->val)
+        //             return false;
+        //         q.push(curr1->left);
+        //         q.push(curr2->right);
+        //         q.push(curr1->right);
+        //         q.push(curr2->left);
+        //     }
+        // }
+        // return true;
     }
 };
