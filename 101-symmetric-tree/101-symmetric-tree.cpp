ba78832ea114fc19b79cbd72d7f9d99b21659508
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2022-09-28 09:48:20 +0530

    Time: 11 ms (42.36%), Space: 16.3 MB (77.39%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -19,26 +19,6 @@ public:
         return func(left->left, right->right) && func(left->right, right->left);
     }
     bool isSymmetric(TreeNode* root) {
-        return !root || func(root->left, root->right);
-        // queue<TreeNode*> q;
-        // q.push(root->left);
-        // q.push(root->right);
-        // while(!q.empty()) {
-        //     TreeNode* curr1 = q.front();
-        //     q.pop();
-        //     TreeNode* curr2 = q.front();
-        //     q.pop();
-        //     if((curr1 && !curr2) || (curr2 && !curr1))
-        //         return false;
-        //     if(curr1 && curr2) {
-        //         if(curr1->val != curr2->val)
-        //             return false;
-        //         q.push(curr1->left);
-        //         q.push(curr2->right);
-        //         q.push(curr1->right);
-        //         q.push(curr2->left);
-        //     }
-        // }
-        // return true;
+        return func(root->left, root->right);
     }
 };
