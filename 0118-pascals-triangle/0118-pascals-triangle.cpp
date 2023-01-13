95ee893b760f9a850f739a53b656b86b192f18a3
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2023-01-13 14:11:42 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (9.37%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        vector<int> v;
+        int value = 1;
+        for(int i = 0; i < numRows; i++) {
+            for(int j = 0; j <= i; j++) {
+                if(i == 0 || j == 0)
+                    value = 1;
+                else
+                    value = value * (i-j+1)/j;
+                v.push_back(value);
+            }
+            ans.push_back(v);
+            v.clear();
+        }
+        return ans;
+    }
+};
