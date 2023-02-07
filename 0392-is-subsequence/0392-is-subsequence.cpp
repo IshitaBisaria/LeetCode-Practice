db9394f9e630d5f9f92851a484d8c2a4cdf7b63d
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2023-02-07 23:12:27 +0530

    Time: 67 ms (5.08%), Space: 286 MB (5.04%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        if(s.size() == 0 || s == t)
+    bool isSubSeq(string s, string t, int m, int n) {
+        if(m == 0)
             return true;
-        if(t.size() == 0)
+        if(n == 0)
             return false;
-        int j = 0;
-        for(int i = 0; i < t.size(); i++) {
-            if(s[j] == t[i]) {
-                j++;
-            }
-            if(j == s.size())
-                return true;
-        }
-        return false;
+        if(s[m-1] == t[n-1])
+            return isSubSeq(s, t, m-1, n-1);
+        else
+            return isSubSeq(s, t, m, n-1);
+    }
+    bool isSubsequence(string s, string t) {
+        // if(s.size() == 0 || s == t)
+        //     return true;
+        // if(t.size() == 0)
+        //     return false;
+        // int j = 0;
+        // for(int i = 0; i < t.size(); i++) {
+        //     if(s[j] == t[i]) {
+        //         j++;
+        //     }
+        //     if(j == s.size())
+        //         return true;
+        // }
+        // return false;
+        int m = s.size();
+        int n = t.size();
+        return isSubSeq(s, t, m, n);
     }
 };
