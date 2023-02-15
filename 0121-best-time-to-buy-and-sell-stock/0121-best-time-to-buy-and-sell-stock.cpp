5f9fd5e68f8a55c04b691ade720a02cf28cbf272
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2023-02-15 11:38:36 +0530

    Time: 123 ms (94.57%), Space: 93.2 MB (91.65%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,8 +5,8 @@ public:
         int mi = INT_MAX;
         for(int i = 0; i < prices.size(); i++) {
             mi = min(mi, prices[i]);
-            int profit = prices[i] - mi;
-            ans = max(ans, profit);
+            int p = prices[i] - mi;
+            ans = max(ans, p);
         }
         return ans;
     }
