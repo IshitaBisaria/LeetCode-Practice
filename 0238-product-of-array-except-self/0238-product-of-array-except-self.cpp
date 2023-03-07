2f4decfe3f6c0845ad8af9f8ab40814d4ca9b6f0
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2023-03-07 19:47:26 +0530

    Time: 35 ms (21.75%), Space: 24.1 MB (55.28%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,9 +4,8 @@ public:
         int n = nums.size();
         vector<int> ans(n);
         ans[0] = 1;
-        for(int i = 1; i < n; i++) {
+        for(int i = 1; i < nums.size(); i++)
             ans[i] = ans[i-1]*nums[i-1];
-        }
         int right = 1;
         for(int i = n-1; i >= 0; i--) {
             ans[i] *= right;
