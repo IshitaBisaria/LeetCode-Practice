69d6d2ecb5cb32fda34d4d3e4d3c3feb87a7df35
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2023-03-07 19:04:27 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (25.93%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     bool wordPattern(string pattern, string s) {
         unordered_map<char, int> mp1;
-        // for(int i = 0; i < pattern.size(); i++)
-        //     mp1[pattern[i]]++;
         unordered_map<string, int> mp2;
         vector<string> arr;
         for(int i = 0; i < s.size(); i++) {
@@ -22,16 +20,5 @@ public:
             mp1[pattern[i]] = mp2[arr[i]] = i+1;
         }
         return true;
-        // for(int i = 0; i < arr.size(); i++)
-        //     cout << arr[i] << " ";
-        // for(int i = 0; i < arr.size(); i++)
-        //     mp2[arr[i]]++;
-        // string st1 = "", st2 = "";
-        // for(auto x : mp1)
-        //     st1 += to_string(x.second);
-        // for(auto x : mp2)
-        //     st2 += to_string(x.second);
-        // cout << st1 << " " << st2 << endl;
-        // return st1 == st2;
     }
 };
