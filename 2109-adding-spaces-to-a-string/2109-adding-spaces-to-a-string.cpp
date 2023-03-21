6c407ce2567a125835ef355f14c2403b105f120f
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2023-03-21 12:56:30 +0530

    Time: 170 ms (74.58%), Space: 81.5 MB (33.38%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
@@ -3,20 +3,12 @@ public:
     string addSpaces(string s, vector<int>& spaces) {
         string ans = "";
         int j = 0;
-        for(int i = 0; i < spaces.size(); i++) {
-            for(; j < s.size(); ) {
-                if(j == spaces[i]) {
-                    ans.push_back(' ');
-                    spaces[i] = -1;
-                    if(i != spaces.size()-1)
-                        break;
-                    // if(i != spaces.size()-1)
-                    //     spaces[i+1]++;
-                } else {
-                    ans.push_back(s[j]);
-                    j++;
-                }
+        for(int i = 0; i < s.size(); i++) {
+            if(j < spaces.size() && i == spaces[j]) {
+                ans += " ";
+                j++;
             }
+            ans += s[i];
         }
         return ans;
     }
