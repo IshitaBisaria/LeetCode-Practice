db82c594c94c609ace4c98e294d6fda17172b590
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2023-08-09 13:32:23 +0530

    Time: 19 ms (84.60%), Space: 19.3 MB (11.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int k = 0;
-        for(int i = 0; i < nums.size(); i++) {
+        for(int i = 0; i < nums.size(); i++)
             if(nums[i] != 0)
                 swap(nums[i], nums[k++]);
-        }
     }
 };
