5593b3aece0d3dab6fddcbfcb662985ca595117f
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2023-08-10 12:11:20 +0530

    Time: 2 ms (46.59%), Space: 8.3 MB (8.41%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low = 0, high = nums.size()-1;
-        int mid = 0;
+        int low = 0, mid = 0, high = nums.size() - 1;
         while(mid <= high) {
-            if(nums[mid] == 0) {
-                swap(nums[mid], nums[low]);
-                low++;
+            if(nums[mid] == 0)
+                swap(nums[low++], nums[mid++]);
+            else if(nums[mid] == 1)
                 mid++;
-            } else if(nums[mid] == 1)
-                mid++;
-            else {
-                swap(nums[mid], nums[high]);
-                high--;
-            }
+            else
+                swap(nums[mid], nums[high--]);
         }
     }
 };
