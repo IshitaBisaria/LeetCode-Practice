896e9b95053fc73c72c9ad45c08a5afcddf6b701
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2023-08-10 12:03:19 +0530

    Time: 3 ms (99.71%), Space: 10.9 MB (23.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,8 +5,8 @@ public:
         vector<int> ans;
         for(int i = 0; i < nums.size(); i++) {
             if(mp.find(target-nums[i]) != mp.end()) {
-                ans.push_back(mp[target-nums[i]]);
                 ans.push_back(i);
+                ans.push_back(mp[target-nums[i]]);
                 return ans;
             }
             mp[nums[i]] = i;
